3cfc79b9bbbc70a908da63f8ef859add0a2cf135
Abbos <abbosibrohimov07@gmail.com>
2023-11-10 19:21:55 +0300

    Type conversions  Opeartors  Comparision  Logical operators

--- script.js
+++ script.js
@@ -1,91 +1,131 @@
-// Number =>
+// let b = 1;
+// let a = "2";
 
-// console.log(123);
-// console.log('123');
+// let str1 = "webbrain";
+// let str2 = "academy";
 
-// let a = b = c = 3;
+// console.log(str1 + str2);
+// console.log(isNaN(a));
 
-// console.log(a, b, c);
+// console.log(b * a);
+// console.log(+a - b);
 
-// let num = 1,
-// a = 2,
-// b = 3;
+// ==========
+// let a = "10";
+// let b = "10.3";
 
-// console.log(num, a, b);
+// console.log(Number(b));
+// console.log(Number.parseInt(b));
+// console.log(Number.parseFloat(b));
+// console.log(parseFloat(b));
+// console.log(parseInt(b));
 
-// ==================================
+// console.log(Number(a));
+// console.log(Number.parseInt(a));
+// console.log(Number.parseFloat(a));
 
-// bigInt =>
-// console.log(99817973636382888n + 1n);
+// ==========
+// true = 1, '1',
+// false -> 0,
+// console.log(false == 0);
+// console.log(true == 1);
+// console.log(false == null);
+// console.log(false == undefined);
+// console.log(undefined == null);
 
-// ==================================
+// console.log(true == "1");
+// console.log(true == "a");
+// console.log(true == "2");
+// console.log(true == "0");
 
+// ==========
+// let a = 1;
+// let b = "10";
 
-// string => '' "" ``
-// let name = "Webbrain Academy"
+// console.log(1 + (1 * 1) / 1);
+// console.log(((1 + 1) * 1) / 1);
 
-// console.log(`Welcome to ${name} IT center`);
+// ========== --, ++
+// a++
+// a = a+1
 
-// console.log("Welcome \nto Webbrain");
-// console.log("Welcome to 'Webbrain'");
-// console.log('Welcome to "Webbrain"');
+// let a = 11; //1
+// console.log(a--); //11
+// console.log(a); // 12
+// console.log(++a); //13
+// console.log(a); // 13
+// console.log(a++); // 13
+// console.log(a++); // 14
+// console.log(++a); // 16
 
-// console.log('Hello JavaScript seperated');
+// ==========
 
-// let text = "WebBrain Academy";
+// Obrivations
+// let a = 50;
 
-// console.log(text);
+// a = a + 10; // 50+10=60
+// a += 10; // 60+10=70
 
-// ==================================
+// console.log(a);
 
+// ==========
+// Comparision
+// let a = 10;
+// let b = 11; //number
+// let c = "11"; // string
 
-// boolean =>
-// console.log(true);
-// console.log(false);
+// =, ==, ==
 
-// ==================================
+// console.log(a > b);
+// console.log(a < b);
+// console.log(c < b);
+// console.log(c <= b);
+// console.log(c >= b);
+// console.log(c == b);
+// console.log(c === b);
+// console.log(c =< b); wrong syn
 
-// undefined vs null =>
-// let a;
-// let b = null;
+// let str1 = "A";
+// let str2 = "a";
 
-// console.log(a, b);
-// console.log(typeof a,typeof b);
-// console.log(null == undefined); => true
-// console.log(null === undefined); => false
-// console.log(null + 1);
-// console.log(undefined + 1);
+// console.log(str1 > str2);
+// console.log(str1 < str2);
+// console.log("sutr1" < "str2");
 
-// let a;
-// let b = null;
+// let a = 1;
+// let b = 1;
+// let check = true;
 
-// console.log(a + 1);
-// console.log(b + 1);
-// console.log(b === b); => true
-// console.log({b} === {b}); => false
+// console.log(a == b); // !=
+// console.log(a === b); //!==
+// console.log(a != b);
+// console.log(check);
+// console.log(!check);
 
-// ==================================
+// ==========
+// Logical Operations
+// OR ||
+// let ceo = false;
+// let admin = true;
 
-// symbol
+// console.log(time === true || ceo === true);
+// console.log(false || true);
+// console.log(admin || ceo);
 
-// let a = Symbol("text");
-// let b = Symbol("text");
+// let time = 11;
+// console.log(admin === true || ceo === true);
 
-// console.log(a == b); => false
-// console.log(a === b); => false
+// AND &&
 
-// ==================================
+// Europe
+let id = false;
+let face = true;
 
-// object --> array obj function
+console.log(id && face);
 
-let arr = ['Abbos', 21, 'Ibrohimov', {}, function test() { }] // index orqali ifodalanadi
+// uzbek
+let galustik = true;
+let soqol = true;
+let pul = true;
 
-let obj = { name: 'Abbos', surname: 'Ibrohimov', age: 21 };  // key orqali ifodalanadi
-
-let func = () => { }
-
-function test() {
-    console.log(arr[0]);
-    console.log(obj.name);
-}
-test()
+console.log(galustik && soqol && pul);
